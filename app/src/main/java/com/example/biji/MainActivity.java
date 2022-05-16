7c13c9f34ea0765f6598a3faf1ce5b51f498a2b3
LAPTOP-EEIF7N7A\木木林 <2373699535@qq.com>
2022-05-16 23:03:31 +0800

    陆轶杰

--- app/src/main/java/com/example/biji/MainActivity.java
+++ app/src/main/java/com/example/biji/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.biji;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -15,7 +20,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements AdapterView.OnItemClickListener {
 
     private NoteDatabase dbHelper;
 
@@ -28,6 +33,8 @@ public class MainActivity extends BaseActivity {
     private NoteAdapter adapter;
     private List<Note> noteList = new ArrayList<Note>();
 
+    private Toolbar myToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,15 +42,25 @@ public class MainActivity extends BaseActivity {
         btn = findViewById(R.id.fab);
         // tv = findViewById(R.id.tv);
         lv = findViewById(R.id.lv);
+        myToolbar = findViewById(R.id.myToolbar);
         adapter = new NoteAdapter(getApplicationContext(), noteList);
         refreshListView();
         lv.setAdapter(adapter);
+
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //设置toolbar取代actionBar
+
+        myToolbar.setNavigationIcon(R.drawable.ic_menu_black_24dp);
+
+        lv.setOnItemClickListener(this);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Log.d(TAG, "onClick: click");
                 Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                intent.putExtra("mode", 4);
                 startActivityForResult(intent, 0);
             }
         });
@@ -52,15 +69,53 @@ public class MainActivity extends BaseActivity {
     //接收startActivtyForResult的结果
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        String content = data.getStringExtra("content");
-        String time = data.getStringExtra("time");
-        Note note = new Note(content, time, 1);
-        CRUD op = new CRUD(context);
-        op.open();
-        op.addNote(note);
-        op.close();
+        int returnMode;
+        long note_Id;
+        returnMode = data.getExtras().getInt("mode", -1);
+        note_Id = data.getExtras().getLong("id", 0);
+
+        if (returnMode == 1) {  //update current note
+
+            String content = data.getExtras().getString("content");
+            String time = data.getExtras().getString("time");
+            int tag = data.getExtras().getInt("tag", 1);
+
+            Note newNote = new Note(content, time, tag);
+            newNote.setId(note_Id);
+            CRUD op = new CRUD(context);
+            op.open();
+            op.updateNote(newNote);
+            op.close();
+        } else if (returnMode == 0) {  // create new note
+            String content = data.getExtras().getString("content");
+            String time = data.getExtras().getString("time");
+            int tag = data.getExtras().getInt("tag", 1);
+
+            Note newNote = new Note(content, time, tag);
+            CRUD op = new CRUD(context);
+            op.open();
+            op.addNote(newNote);
+            op.close();
+        }else if (returnMode == 2) { // delete
+            Note curNote = new Note();
+            curNote.setId(note_Id);
+            CRUD op = new CRUD(context);
+            op.open();
+            op.removeNote(curNote);
+            op.close();
+        }else{
+
+        }
         refreshListView();
+        super.onActivityResult(requestCode, resultCode, data);
+        //        String content = data.getStringExtra("content");
+//        String time = data.getStringExtra("time");
+//        Note note = new Note(content, time, 1);
+//        CRUD op = new CRUD(context);
+//        op.open();
+//        op.addNote(note);
+//        op.close();
+//        refreshListView();
 
     }
 
@@ -75,4 +130,26 @@ public class MainActivity extends BaseActivity {
         adapter.notifyDataSetChanged();
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (parent.getId()) {
+            case R.id.lv:
+                Note curNote = (Note) parent.getItemAtPosition(position);
+                Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                intent.putExtra("content", curNote.getContent());
+                intent.putExtra("id", curNote.getId());
+                intent.putExtra("time", curNote.getTime());
+                intent.putExtra("mode", 3);     // MODE of 'click to edit'
+                intent.putExtra("tag", curNote.getTag());
+                startActivityForResult(intent, 1);      //collect data from edit
+                Log.d(TAG, "onItemClick: " + position);
+                break;
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
 }

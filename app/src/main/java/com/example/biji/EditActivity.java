38a750ab865176678372c4ef09c649652fd1c3ce
LAPTOP-EEIF7N7A\木木林 <2373699535@qq.com>
2022-05-16 19:03:42 +0800

    lyw001

--- app/src/main/java/com/example/biji/EditActivity.java
+++ app/src/main/java/com/example/biji/EditActivity.java
@@ -1,12 +1,48 @@
 package com.example.biji;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-public class EditActivity extends AppCompatActivity{
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class EditActivity extends BaseActivity {
+
+    EditText et;
+    private String content;
+    private String time;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.edit_layout);
+        et = findViewById(R.id.et);
+
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if (keyCode == KeyEvent.KEYCODE_HOME){
+            return true;
+        }
+        else if (keyCode == KeyEvent.KEYCODE_BACK){
+            Intent intent = new Intent();
+            intent.putExtra("content", et.getText().toString());
+            intent.putExtra("time", dateToStr());
+            setResult(RESULT_OK, intent);
+            finish();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public String dateToStr(){
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return simpleDateFormat.format(date);
     }
 }
--- app/src/main/java/com/example/biji/MainActivity.java
+++ app/src/main/java/com/example/biji/MainActivity.java
@@ -2,13 +2,77 @@ package com.example.biji;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends BaseActivity {
+
+    private NoteDatabase dbHelper;
+
+    private Context context = this;
+    final String TAG = "tag";
+    FloatingActionButton btn;
+    TextView tv;
+    private ListView lv;
+
+    private NoteAdapter adapter;
+    private List<Note> noteList = new ArrayList<Note>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.fab);
+        // tv = findViewById(R.id.tv);
+        lv = findViewById(R.id.lv);
+        adapter = new NoteAdapter(getApplicationContext(), noteList);
+        refreshListView();
+        lv.setAdapter(adapter);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Log.d(TAG, "onClick: click");
+                Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
+
+    //接收startActivtyForResult的结果
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        String content = data.getStringExtra("content");
+        String time = data.getStringExtra("time");
+        Note note = new Note(content, time, 1);
+        CRUD op = new CRUD(context);
+        op.open();
+        op.addNote(note);
+        op.close();
+        refreshListView();
+
+    }
+
+    public void refreshListView(){
+
+        CRUD op = new CRUD(context);
+        op.open();
+        // set adapter
+        if (noteList.size() > 0) noteList.clear();
+        noteList.addAll(op.getAllNotes());
+        op.close();
+        adapter.notifyDataSetChanged();
+    }
+
 }

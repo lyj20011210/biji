220efb25c409bd8fa7ae76e93ad8ec6cabefe805
LAPTOP-FENG\Feng_Ming_ <20202005417@m.scnu.edu.cn>
2022-05-19 19:48:10 +0800

    zhang: delete and search

--- app/src/main/java/com/example/biji/EditActivity.java
+++ app/src/main/java/com/example/biji/EditActivity.java
@@ -1,10 +1,13 @@
 package com.example.biji;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.View;
 import android.widget.EditText;
 
 import java.text.SimpleDateFormat;
@@ -13,31 +16,98 @@ import java.util.Date;
 public class EditActivity extends BaseActivity {
 
     EditText et;
-    private String content;
-    private String time;
+    // private String content;
+    // //private String time;
+
+    private Toolbar myToolbar;
+    private String old_content = "";
+    private String old_time = "";
+    private int old_Tag = 1;
+    private long id = 0;
+    private int openMode = 0;
+    private int tag = 1;
+    public Intent intent = new Intent(); // message to be sent
+    private boolean tagChange = false;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_layout);
-        et = findViewById(R.id.et);
+        myToolbar = findViewById(R.id.my_Toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //设置toolbar取代actionbar
 
+        myToolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                autoSetMessage();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+
+        et = findViewById(R.id.et);
+        Intent getIntent = getIntent();
+        openMode = getIntent.getIntExtra("mode", 0);
+
+        if (openMode == 3) {//打开已存在的note
+            id = getIntent.getLongExtra("id", 0);
+            old_content = getIntent.getStringExtra("content");
+            old_time = getIntent.getStringExtra("time");
+            old_Tag = getIntent.getIntExtra("tag", 1);
+            et.setText(old_content);
+            et.setSelection(old_content.length());
+
+        }
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 
     public boolean onKeyDown(int keyCode, KeyEvent event){
         if (keyCode == KeyEvent.KEYCODE_HOME){
             return true;
         }
         else if (keyCode == KeyEvent.KEYCODE_BACK){
-            Intent intent = new Intent();
-            intent.putExtra("content", et.getText().toString());
-            intent.putExtra("time", dateToStr());
+            autoSetMessage();
+            //Intent intent = new Intent();
+            //intent.putExtra("content", et.getText().toString());
+            //intent.putExtra("time", dateToStr());
             setResult(RESULT_OK, intent);
             finish();
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    public void autoSetMessage(){
+        if(openMode == 4){
+            if(et.getText().toString().length() == 0){
+                intent.putExtra("mode", -1); //nothing new happens.
+            }
+            else{
+                intent.putExtra("mode", 0); // new one note;
+                intent.putExtra("content", et.getText().toString());
+                intent.putExtra("time", dateToStr());
+                intent.putExtra("tag", tag);
+            }
+        }
+        else {
+            if (et.getText().toString().equals(old_content) && !tagChange)
+                intent.putExtra("mode", -1); // edit nothing
+            else {
+                intent.putExtra("mode", 1); //edit the content
+                intent.putExtra("content", et.getText().toString());
+                intent.putExtra("time", dateToStr());
+                intent.putExtra("id", id);
+                intent.putExtra("tag", tag);
+            }
+        }
     }
 
     public String dateToStr(){
--- app/src/main/java/com/example/biji/Note.java
+++ app/src/main/java/com/example/biji/Note.java
@@ -6,6 +6,7 @@ public class Note {
     private String time;
     private int tag;
 
+
     public Note(){
 
     }
